45a594526904cf030b0e0129729be874eb88743b
mandarinnn2 <mandarinnn2@o.cnu.ac.kr>
2024-10-14 17:51:54 +0900

    동일한 내용의 배열이지만 주소가 다름

---
+++ Check.java
@@ -0,0 +1,19 @@
+class Check{
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 2, 3, 4};
+        int[] b = new int[]{1, 2, 3, 4};
+        int[] c = new int[]{1, 2, 3};
+
+        check(a, b);
+        check(a, c);
+        check(b, c);
+    }
+    public static void check(int[] a, int[] b){
+        if(a==b)
+            System.out.print("O");
+        else
+            System.out.print("N");
+    }
+}
+
+// a와 b의 배열 내용은 같지만 서로 다른 주소값을 갖고 있기 때문에 모두 N이 출력된다.
